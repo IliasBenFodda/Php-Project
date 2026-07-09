33ea1a057224351554090f2fc518a796b025df8c
Ben-Fodda <ilias.benfodda@student.odisee.be>
2026-07-09 16:30:47 +0200

    configure database connection

---
+++ database/factories/FaqFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Faq;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Faq>
+ */
+class FaqFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'question' => fake()->sentence() . '?',
+            'answer' => fake()->paragraph(),
+            'category_id' => Category::factory(),
+            'user_id' => null,
+        ];
+    }
+}
